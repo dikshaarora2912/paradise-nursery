246c7e229ceb6dd6dba87b29f6a1d7714b1272d0
Ankit Mehra <ankitmehra@Ankits-MacBook-Air.local>
2026-04-01 17:08:01 +0530

    Added CartSlice.jsx

---
+++ src/redux/CartSlice.jsx
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [], 
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      const item = action.payload;
+      const existingItem = state.items.find((i) => i.id === item.id);
+
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ ...item, quantity: 1 });
+      }
+    },
+    removeItem: (state, action) => {
+      const id = action.payload;
+      state.items = state.items.filter((i) => i.id !== id);
+    },
+    updateQuantity: (state, action) => {
+      const { id, quantity } = action.payload;
+      const item = state.items.find((i) => i.id === id);
+      if (item) {
+        item.quantity = quantity;
+      }
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = cartSlice.actions;
+export default cartSlice.reducer;
